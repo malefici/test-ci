5de9bdc5b65b3a8c9a55bc91d9f3b5175a9e63a9
KitCat <kitcat@malefici.com>
2023-12-12 23:25:10 +0200

    Initial commit

---
+++ src/Controller/MicroController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Malefici\TestCi\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MicroController extends AbstractController
+{
+    #[Route('/random/{limit}')]
+    public function randomNumber(int $limit): Response
+    {
+        $number = random_int(0, $limit);
+
+        return $this->render('micro/random.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
---
+++ tests/BaseUnitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Malefici\TestCi\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseUnitTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSomething2(): void
+    {
+        $this->assertTrue(true);
+    }
+}
